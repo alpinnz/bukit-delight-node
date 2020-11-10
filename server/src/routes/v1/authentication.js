6e30c5963c42e0194071fe5a068c4a1bea04be4c
Alpinnz <alpinnz@gmail.com>
2020-11-10 23:01:20 +0700

    create login register forgot password

--- server/src/routes/v1/authentication.js
+++ server/src/routes/v1/authentication.js
@@ -1,5 +1,5 @@
 const Router = require("express").Router();
-const Response = require("../../middlewares/Response");
+const Response = require("./../../middlewares/Response");
 
 Router.post("/login", async (req, res) => {
   const {} = req.body;
@@ -8,7 +8,12 @@ Router.post("/login", async (req, res) => {
 
 Router.post("/register", async (req, res) => {
   const {} = req.body;
-  return Response.Success(res, "register");
+  return Response.Success(res, "Register");
+});
+
+Router.post("/forgot-password", async (req, res) => {
+  const {} = req.body;
+  return Response.Success(res, "Forgot Password");
 });
 
 module.exports = Router;
