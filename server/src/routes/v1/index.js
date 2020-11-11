b0583759e395e11ee6444840189d76c7efe61901
Alpinnz <alpinnz@gmail.com>
2020-11-11 12:03:34 +0700

    fix

--- server/src/routes/v1/index.js
+++ server/src/routes/v1/index.js
@@ -1,32 +1,12 @@
-const Router = require("express").Router();
-const { Authentication } = require("./../../middlewares");
+const express = require("express");
+const router = express.Router();
 
-Router.get("/", Authentication.checkAccessToken, (req, res, next) => {
-  res.json({ index: "index" });
+router.get("/", async (req, res) => {
+  const { } = req.body;
+  res.send("api");
 });
 
-const authentication = require("./authentication");
-Router.use("/authentication", authentication);
+const v1 = require("./v1");
+router.use("/v1", v1);
 
-const accounts = require("./accounts");
-Router.use("/accounts", accounts);
-
-const categories = require("./categories");
-Router.use("/categories", categories);
-
-const menus = require("./menus");
-Router.use("/menus", menus);
-
-const tables = require("./tables");
-Router.use("/tables", tables);
-
-const transactions = require("./transactions");
-Router.use("/transactions", transactions);
-
-const orders = require("./orders");
-Router.use("/orders", orders);
-
-const itemOrders = require("./itemOrders");
-Router.use("/item-orders", itemOrders);
-
-module.exports = Router;
+module.exports = router;
