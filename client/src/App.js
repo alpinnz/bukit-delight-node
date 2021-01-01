b2f37998c3b8ef58be72cd162181c505b005f1cd
Alpinnz <alpinnz@gmail.com>
2021-01-01 12:04:38 +0700

    init change

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,25 @@
-import * as React from "react";
-import {
-  CssBaseline,
-  createMuiTheme,
-  ThemeProvider,
-  responsiveFontSizes,
-} from "@material-ui/core";
-import { Provider } from "react-redux";
-import Routes from "./routes";
-import Store from "./redux";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
-let theme = createMuiTheme({
-  typography: {
-    fontFamily: `'Roboto', sans-serif`,
-    fontSize: 14,
-    fontWeightLight: 300,
-    fontWeightRegular: 400,
-    fontWeightMedium: 500,
-  },
-  overrides: {
-    MuiCssBaseline: {
-      "@global": {
-        "@font-face": [],
-      },
-    },
-  },
-});
-
-theme = responsiveFontSizes(theme);
-
-const App = () => {
+function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <CssBaseline />
-        <Provider store={Store}>
-          <Routes />
-        </Provider>
-      </div>
-    </ThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
