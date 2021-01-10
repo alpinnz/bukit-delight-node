7e29522b246ad37a60a43ad7791b20b36fd24d91
Alpinnz <alpinnz@gmail.com>
2021-01-10 07:27:51 +0700

    no message

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,36 +1,12 @@
 module.exports = {
-  /**
-   * Application configuration section
-   * http://pm2.keymetrics.io/docs/usage/application-declaration/
-   */
   apps: [
     {
-      name: "my_react_app",
+      name: "bukit-delight-client",
       script: "npm",
-      args: "run start:production",
       env_production: {
+        PORT: 80,
         NODE_ENV: "production",
       },
     },
   ],
-
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-   */
-  deploy: {
-    production: {},
-    staging: {
-      user: "your-user",
-      host: "your-server",
-      ref: "origin/master",
-      repo: "git@github.com:gituser/yourrepo.git",
-      path: "/var/www/yourprojectpath",
-      key: "/absolute/path/to/key",
-      ssh_options: ["ForwardAgent=yes"],
-      "post-deploy":
-        "npm install && pm2 reload ecosystem.config.js --env production",
-    },
-    dev: {},
-  },
 };
