007c610a557a52a7e22ef648dbbd72f546412339
Alpinnz <alpinnz@gmail.com>
2021-01-17 14:22:23 +0700

    up ip server

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ const server = http.createServer(app);
 
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:80",
+    origin: "http://139.180.154.222:80",
     methods: ["GET", "POST"],
   },
 });
